50b4505aec91888d64545a3ab9222ca38d4005e7
marcushu <marcushurn@hotmail.com>
2021-03-01 11:51:42 -0800

    cleaned up Roster

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -7,63 +7,56 @@ import Table from 'react-bootstrap/Table';
 import PropTypes from 'prop-types';
 
 import './roster.css';
+import { useState } from 'react';
 
-class Roster extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      selectedPlayerID: null,
-      hittingStats: null,
-      seasonHittingStats: null,
-      pitchingStats: null,
-      seasonPitchingState: null
+const Roster = ({ roster }) => {
+  const [selectedPlayerId, setselectedPlayerId] = useState();
+  const [hittingStats, sethittingStats] = useState();
+  const [seasonHittingStats, setseasonHittingStats] = useState();
+  const [pitchingStats, setpitchingStats] = useState();
+  const [seasonPitchingState, setseasonPitchingState] = useState();
+
+  const highlightPlayer = async playerId => {
+    try {
+      const hittingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerId}%27`);
+      const pitchingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerId}%27`);
+      const seasonHittingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%272017%27&player_id=%27${playerId}%27`);
+      const seasonPitchingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%272017%27&player_id=%27${playerId}%27`);
+
+      sethittingStats(hittingRes.data.sport_career_hitting.queryResults)
+      setseasonHittingStats(seasonHittingRes.data.sport_hitting_tm.queryResults);
+      setpitchingStats(pitchingRes.data.sport_career_pitching.queryResults);
+      setseasonPitchingState(seasonPitchingRes.data.sport_pitching_tm.queryResults);
+      setselectedPlayerId(playerId);
+    } catch (error) {
+      console.error(error);
     }
 
-    Roster.propTypes = {
-      roster: PropTypes.array.isRequired
-    }
   }
 
-  highlightPlayer = async playerId => {
-    const hittingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerId}%27`);
-    const pitchingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27${playerId}%27`);
-    const seasonHittingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%272017%27&player_id=%27${playerId}%27`);
-    const seasonPitchingRes = await axios.get(`https://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id=%27mlb%27&game_type=%27R%27&season=%272017%27&player_id=%27${playerId}%27`);
-
-    this.setState({
-      selectedPlayerID: playerId,
-      hittingStats: hittingRes.data.sport_career_hitting.queryResults,
-      seasonHittingStats: seasonHittingRes.data.sport_hitting_tm.queryResults,
-      pitchingStats: pitchingRes.data.sport_career_pitching.queryResults,
-      seasonPitchingState: seasonPitchingRes.data.sport_pitching_tm.queryResults
-    });
-  }
-
-  render() {
-    return (
-      <Row>
-        <Col>
+  return (
+    <Row>
+      <Col>
         <div className="table-responsive">
           <Table size="sm">
             <thead>
-              <tr style={{backgroundColor: "rgb(212, 225, 255)"}}>
+              <tr style={{ backgroundColor: "rgb(212, 225, 255)" }}>
                 <th></th>
                 <th>Position</th>
                 <th>Height</th>
-
                 <th>Weight</th>
                 <th>Pro Debut</th>
               </tr>
             </thead>
             <tbody>
-              {this.props.roster.map(player =>
+              {roster.map(player =>
                 <React.Fragment key={player.player_id}>
                   <tr>
                     <td>
-                      <button onClick={(e) => this.highlightPlayer(player.player_id, e)}>
-                        {player.name_display_first_last} 
-                        <span id="number"> {player.jersey_number}</span> 
+                      <button onClick={(e) => highlightPlayer(player.player_id, e)}>
+                        {player.name_display_first_last}
+                        <span id="number"> {player.jersey_number}</span>
                       </button>
                     </td>
                     <td>
@@ -72,7 +65,6 @@ class Roster extends React.Component {
                     <td>
                       {player.height_feet}'{player.height_inches}"
                     </td>
-
                     <td>
                       {player.weight}
                     </td>
@@ -80,15 +72,14 @@ class Roster extends React.Component {
                       {player.pro_debut_date.substr(0, 4)}
                     </td>
                   </tr>
-                  {/* conditional */}
-                  {player.player_id === this.state.selectedPlayerID ?
+                  {player.player_id === selectedPlayerId ?
                     <tr>
                       <td colSpan={5}>
                         <PlayerDetails
-                          hitting={this.state.hittingStats}
-                          pitching={this.state.pitchingStats}
-                          seasonHitting={this.state.seasonHittingStats}
-                          seasonPitching={this.state.seasonPitchingState}
+                          hitting={hittingStats}
+                          pitching={pitchingStats}
+                          seasonHitting={seasonHittingStats}
+                          seasonPitching={seasonPitchingState}
                         />
                       </td>
                     </tr> : <></>
@@ -97,14 +88,14 @@ class Roster extends React.Component {
               )}
             </tbody>
           </Table>
-          </div>
-        </Col>
-      </Row >
-    )
-  }
-
+        </div>
+      </Col>
+    </Row >
+  )
 }
 
-
+Roster.propTypes = {
+  roster: PropTypes.array.isRequired
+}
 
 export default Roster;
